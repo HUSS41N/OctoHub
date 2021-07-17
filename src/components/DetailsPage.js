983b2e83477bb1345dca0a2a1e4b7f7a379b502c
Md Hussain <hussainakhtar1111@gmail.com>
2021-07-17 16:13:44 +0530

    + more details & improved UI + icons

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,19 +1,53 @@
 import React,{useContext} from "react"
+import {IoLocationOutline} from "react-icons/io5"
+import {CgCalendarDates} from "react-icons/cg"
 import UserContext from "../context/UserContext"
-import logo from "../logo/Octocat.png"
 import styled  from "styled-components"
+import { Redirect } from "react-router"
 const DetailsPage = () => {
     const [user,setUser] = useContext(UserContext)
+    if(!user){
+        return <Redirect to="/"/>
+    }
     return (
+        <div>
         <DetailsDiv>
             <img src={user.data.avatar_url} alt="placeholder"/>
             <h1>{user.data.name}</h1>
             <a href={user.data.html_url}>@{user.data.login}</a>
             <div>
-                <p>{user.data.location}</p>
-                <p>{user.data.created_at}</p>
+                <p> 
+                    <IoLocationOutline/>
+                    {" "}
+                    { user.data.location}
+                    {"    "}
+                </p>
+                <p>
+                    <CgCalendarDates/>
+                    {" "}joined{" "}
+                    {new Date(user.data.created_at).toLocaleDateString("en-US",{
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric'
+                })}
+                </p>
             </div>
+            <StatDiv>
+            <div>
+                <h2>{user.data.followers}</h2>
+                <p>Followers</p>
+            </div>
+            <div>
+                <h2>{user.data.public_repos}</h2>
+                <p>Repositories</p>
+            </div>
+            <div>
+                <h2>{user.data.following}</h2>
+                <p>Following</p>
+            </div>
+        </StatDiv>
         </DetailsDiv>
+        </div>
     )
 }
 
@@ -47,7 +81,23 @@ const DetailsDiv = styled.div`
         color: #97EAD2;
     }
     div p {
-        margin-left: 2rem;
+        margin: 1rem;
     }
     `
+const StatDiv = styled.div`
+    div{
+        display: flex ;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: #8CC7A1;
+        color: #74226C;
+        padding: 1rem;
+        border-radius: 10px;
+        margin: 2rem;
+        p{
+            font-size: 16px;
+        }
+    }
+`
 export default DetailsPage;
