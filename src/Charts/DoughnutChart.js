2efb4a6fca9cbeeb406557b2fd7b91aa47386a31
Md Hussain <hussainakhtar1111@gmail.com>
2021-07-21 15:54:17 +0530

    + chart div style

--- src/Charts/DoughnutChart.js
+++ src/Charts/DoughnutChart.js
@@ -78,5 +78,17 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: #fff;
+  border-radius: 2px;
+  display: inline-block;
+  height: 350px;
+  margin: 1rem;
+  position: relative;
+  width: 400px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+  :hover{
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
 `;
 export default DoughnutChart;
